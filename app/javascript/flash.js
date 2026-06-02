188f9d6df705d7bb66a3d541e1b6e2a8f603a86a
Dom Pukallus <dpukallus9@gmail.com>
2026-06-02 16:35:58 +1000

    merging master

---
+++ app/javascript/flash.js
@@ -0,0 +1,14 @@
+document.addEventListener("turbo:load", () => {
+  const flash = document.getElementById("flash-message");
+
+  if (flash) {
+    setTimeout(() => {
+      flash.style.transition = "opacity 0.5s ease";
+      flash.style.opacity = "0";
+
+      setTimeout(() => {
+        flash.remove();
+      }, 500);
+    }, 5000);
+  }
+});
