388fdce6b5ca1e91fbb1c570aad4bf81f8ca5811
Chloe de Mendonca <chloe.de_mendonca@edu.escp.eu>
2026-06-09 17:02:54 +1000

    Cateory page made and updated

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,4 +7,19 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     @subscriptions = @category.subscriptions.where(user: current_user).order(:name)
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = current_user.categories.new(category_params)
+    if @category.save
+      redirect_to categories_path, notice: "category created"
+    else
+      @mode = "manual"
+      flash.now[:alert] = "Failed to add category"
+      render "categories/new", status: :unprocessable_entity
+    end
+  end
 end
