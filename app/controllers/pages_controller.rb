af2983886c6ee6d0ff6b153fde5be61c4031d2f3
Jun Tomojiri <yo.uk.ao.9095@gmail.com>
2026-06-10 14:25:52 +1000

    I make landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: :landing
 
   def home
+  end
+
+  def landing
+    redirect_to home_path if user_signed_in?
   end
 end
