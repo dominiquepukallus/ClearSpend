e70a93fd77264b8014ab7257f03abc30eea94686
Chloe de Mendonca <chloe.de_mendonca@edu.escp.eu>
2026-06-11 12:15:01 +1000

    Lots of improvements

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -16,15 +16,27 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     @category.destroy
     respond_to do |format|
-      format.turbo_stream { render turbo_stream: turbo_stream.remove("category_#{@category.id}") }
-      format.html { redirect_to categories_path }
+      format.turbo_stream
+      format.html { redirect_to subscriptions_path }
     end
   end
 
+  def bulk_destroy
+    seeded_names = ["Entertainment", "Well-being", "News & Information", "Food & Delivery / Meal kits",
+                    "Software & Productivity", "Shopping & Retail"]
+    Category.where(id: params[:category_ids]).where.not(name: seeded_names).destroy_all
+    redirect_to subscriptions_path, notice: "Categories deleted!"
+  end
+
   def create
+    if Category.where.not(name: ["Entertainment", "Well-being", "News & Information", "Food & Delivery / Meal kits",
+                                 "Software & Productivity", "Shopping & Retail", "Customized"]).count >= 6
+      redirect_to subscriptions_path, alert: "You have reached the maximum of 6 custom categories."
+      return
+    end
     @category = Category.new(category_params)
     if @category.save
-      redirect_to categories_path, notice: "Category created!"
+      redirect_to subscriptions_path, notice: "Category created!"
     else
       render :new, status: :unprocessable_entity
     end
