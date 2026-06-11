68469522d0aaff85a6708a29bf27f557bc74d28e
Dom Pukallus <dpukallus9@gmail.com>
2026-06-11 15:31:18 +1000

    pushing styling changes to master

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,14 @@ class Message < ApplicationRecord
   belongs_to :chat
 
   validates :content, :role, presence: true
+
+  after_create :update_chat_title_if_first_message
+
+  private
+
+  def update_chat_title_if_first_message
+    if role == "user" && chat.messages.where(role: "user").count == 1
+      chat.update(title: content.truncate(50, omission: "..."))
+    end
+  end
 end
